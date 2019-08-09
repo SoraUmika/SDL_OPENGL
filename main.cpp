1fbe25b7361ae827d6de1ff2f5e9af0f9e23c0c6
SoraUmika <35876897+SoraUmika@users.noreply.github.com>
2019-08-09 17:00:24 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
 #include <SDL.h>
 #include <stdio.h>
+#include <Game.h>
+
 using namespace std;
 
 int main(int argv, char** args){
 
-    SDL_Window* gWindow = NULL;
-    SDL_Surface* gWindowSurface = NULL;
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 600;
+    Game gameApp = Game();
 
-    //initialize SDL
-    if(SDL_Init( SDL_INIT_VIDEO ) < 0){
-        cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << endl;
-    }else{
-        gWindow = SDL_CreateWindow("SDL_OPENGL", SDL_WINDOWPOS_UNDEFINED, 
-        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-
-        if(gWindow == NULL){
-            cout << "Windows cannot be intialized! SDL Error: " << SDL_GetError() << endl;
-        }else{
-            gWindowSurface = SDL_GetWindowSurface( gWindow );
-        }
+    while(!gameApp.exit_status){
+        gameApp.mainLoop();
     }
     return 0;   
 }       
